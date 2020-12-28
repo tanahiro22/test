612ba72c86eb1242b3aa74d5453a8e2ee6793840
hiroki tanaka <hiroki.tanaka@findex.co.jp>
2020-12-28 09:39:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button) as Button
+
+
+        button.setOnClickListener{
+            Toast.makeText(this, "クリックされました", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+}
